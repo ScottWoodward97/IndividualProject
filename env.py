312e909c11444a780f6a803c51b6ea2cd2f6c81d
scottwoodward97 <sw652@exeter.ac.uk>
2019-03-06 17:02:25 +0000

    Env now runs 500 games without updating to generate data for statistical analysis and comparison of players

--- env.py
+++ env.py
@@ -4,56 +4,56 @@ from operator import add
 
 from golf import Golf, Golf_Analyser
 from player import Golf_Player, Random_Golf_Player, Greedy_Golf_Player
-import file_write as fw
+import function_approximator as fa
 import pickle
 
 
 g = Golf()
 
-#DIR_PATH_PLAYERS = os.path.join('games', 'coevo', 'test_3')
-DIR_PATH_SAVE = os.path.join('games', 'analysis', 'random')
+DIR_PATH = os.path.join('games', 'coevo_score_with_random', 'one_hot_hand_1')
+DIR_PATH_SAVE = os.path.join('games', 'coevo_score_with_random_analysis', 'one_hot_hand_1')
 
 if not os.path.exists(DIR_PATH_SAVE):
     os.makedirs(DIR_PATH_SAVE)
+if not os.path.exists(os.path.join(DIR_PATH_SAVE, "training")):
+    os.makedirs(os.path.join(DIR_PATH_SAVE, "training"))
+if not os.path.exists(os.path.join(DIR_PATH_SAVE, "random")):
+    os.makedirs(os.path.join(DIR_PATH_SAVE, "random"))
 
-#with open(os.path.join(DIR_PATH_PLAYERS, 'PLAYER_PICKLE.p'), 'rb') as f:
-#    p1 = pickle.load(f)
-#with open(os.path.join(DIR_PATH_PLAYERS, 'OPPONENT_PICKLE.p'), 'rb') as f:
-#    p2 = pickle.load(f)
-p1 = Random_Golf_Player()
-p2 = Random_Golf_Player()
+with open(os.path.join(DIR_PATH, 'PLAYER_PICKLE.p'), 'rb') as f:
+    player = pickle.load(f)
+with open(os.path.join(DIR_PATH, 'OPPONENT_PICKLE.p'), 'rb') as f:
+    opponent = pickle.load(f)
 
+random_player = Random_Golf_Player()
 
-
-GAME = ["",""]
+GAMES = ["",""]
+RANDOM_GAMES = ["",""]
 t = time.time()
-for _ in range(10000):
-    game = g.play_pair(p1, p2)
+for n in range(2500):
+    games = g.play_pair(player, opponent)
+    GAMES = list(map(add, GAMES, games))
+    GAMES[0]+='\n'
+    GAMES[1]+='\n' 
 
-    GAME = list(map(add, GAME, game))
-    GAME[0]+='\n'
-    GAME[1]+='\n' 
+    random_games = g.play_pair(player, random_player)
+    RANDOM_GAMES = list(map(add, RANDOM_GAMES, random_games))
+    RANDOM_GAMES[0]+='\n'
+    RANDOM_GAMES[1]+='\n'
+
+    if (n+1)%125 == 0:
+        print(n+1)   
+        filename = time.strftime("%Y%m%d-%H%M%S-", time.gmtime())
+        for i in range(2):
+            with open(os.path.join(DIR_PATH_SAVE, "training", '%s%d.txt' % (filename, i)), 'w+') as f: #look at a+ instead, will append if the file exists
+                f.write(GAMES[i])
+            with open(os.path.join(DIR_PATH_SAVE, "random", '%s%d.txt' % (filename, i)), 'w+') as f: #look at a+ instead, will append if the file exists
+                f.write(RANDOM_GAMES[i])
+        GAMES = ["",""]
+        RANDOM_GAMES = ["",""]
 
 
 
 t_2 = time.time()
 
-#print(os.getcwd())
-
-#if not os.path.exists('games'):
-#    os.makedirs('games')
-
-for g in range(2):
-    with open(os.path.join(DIR_PATH_SAVE, 'test_game_%d.txt'%g), 'w+') as f:
-        f.write("".join(GAME[g]))
-#    fw.write_game(None, None)
-
-#    with open(os.path.join('games', 'test_game%d_1.txt'%g), 'w+') as f:
-#        f.write(GAME[g][1])
-#    fw.write_game(None, None)
-
-
-#print(GAME)
-#print(Golf_Analyser.extract_scores(GAME[0]))
-#print(Golf_Analyser.extract_scores(GAME[1]))
 print(t_2-t)
