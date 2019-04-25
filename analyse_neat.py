f76035baa21f6601e2f64d3dee8588893698d3e0
scottwoodward97 <sw652@exeter.ac.uk>
2019-04-25 01:08:24 +0100

    Finalised docstrings and comments

--- analyse_neat.py
+++ analyse_neat.py
@@ -1,3 +1,9 @@
+"""
+---analyse_neat.py---
+Contains the methods that analyse the population data of the NEAT training process.
+These plot graphs that map the metrics of the best and worst solutions (fitness) in the population as well as the population average.
+Where relevant, the highest and lowest of each metric in the population is also plotted.
+"""
 from golf import Golf_Analyser
 import glob
 import os
@@ -5,17 +11,27 @@ import numpy as np
 
 
 def plot_mean_scores(dir_path, games_per_solution=10):
+    """
+    Plots the mean scores at each generation obtained by the population during training.
+    The mean scores of the best and worst players in the population as well as the population average.
+    Args:
+        dir_path (String): The directory of the player trained under NEAT.
+        games_per_solution(int): The number of games each solution plays in its fitness calculation. Default set to 10.
+    Returns: None
+    """
+    #Load the paths of each generation's game file from the relevant directory
     files = glob.glob('%s/game_files/*.txt' % dir_path)
     best_solution, worst_solution, pop_av = [],[],[]
 
     for game_file in files:
         with open(game_file, "r") as f:
             games = f.read().split('\n\n')[:-1]
+        #Extracts the scores from the games
         all_scores = [Golf_Analyser.extract_scores(game + '\n') for game in games]
         solution_scores = np.reshape(all_scores, (len(all_scores)//games_per_solution, games_per_solution, 2))
         
         mean_solution_scores = np.mean(np.amin(solution_scores, axis=2), axis=1)
-        
+        #Extracts the best solution's, worst solution's and the population average score
         best_solution.append(float(min(mean_solution_scores)))
         worst_solution.append(float(max(mean_solution_scores)))
         pop_av.append(float(np.mean(mean_solution_scores)))
@@ -23,12 +39,23 @@ def plot_mean_scores(dir_path, games_per_solution=10):
     Golf_Analyser.plot_data(list(zip(best_solution, worst_solution, pop_av)), 3, "Generations", "Mean Score", "Mean Score over Generations", "Best Solution", "Worst Solution", "Population Average")
 
 def plot_mean_number_of_turns(dir_path, games_per_solution=10):
+    """
+    Plots the mean number of turns at each generation obtained by the population during training.
+    The mean number of turns of the best and worst players in the population (by fitness) as well as the population average.
+    The highest and lowest number of turns by solutions in the popualtion are also plotted.
+    Args:
+        dir_path (String): The directory of the player trained under NEAT.
+        games_per_solution(int): The number of games each solution plays in its fitness calculation. Default set to 10.
+    Returns: None
+    """
+    #Load the paths of each generation's game file from the relevant directory
     files = glob.glob('%s/game_files/*.txt' % dir_path)
     best_solution, worst_solution, pop_av, highest, lowest = [],[],[], [], []
 
     for game_file in files:
         with open(game_file, "r") as f:
             games = f.read().split('\n\n')[:-1]
+        #Extracts the scores and number of turns from each file
         all_scores = [Golf_Analyser.extract_scores(game + '\n') for game in games]
         all_turns = [sum(Golf_Analyser.extract_number_of_turns(game + '\n')) for game in games]
 
@@ -37,9 +64,11 @@ def plot_mean_number_of_turns(dir_path, games_per_solution=10):
         
         mean_solution_scores = np.mean(np.amin(solution_scores, axis=2), axis=1)
 
+        #By using the scores, locate the best and worst solution at each generation and add their metrics
         best_solution.append(float(solution_turns[np.argmin(mean_solution_scores)]/(9*games_per_solution)))
         worst_solution.append(float(solution_turns[np.argmax(mean_solution_scores)]/(9*games_per_solution)))
 
+        #Extract the highest, lowest and average number of turns obtained by solutions in the population
         lowest.append(float(min(solution_turns)/(9*games_per_solution)))
         highest.append(float(max(solution_turns)/(9*games_per_solution)))
         pop_av.append(float(np.mean(solution_turns)/(9*games_per_solution)))
@@ -47,12 +76,23 @@ def plot_mean_number_of_turns(dir_path, games_per_solution=10):
     Golf_Analyser.plot_data(list(zip(best_solution, worst_solution, pop_av, highest, lowest)), 5, "Generations", "Average Number of Turns per Round", "Average Number of Turns per Round over Generations", "Best Solution", "Worst Solution", "Population Average", "Population Highest", "Population Lowest")
 
 def plot_mean_number_of_matches(dir_path, games_per_solution=10):
+    """
+    Plots the mean number of matches made at each generation obtained by the population during training.
+    The mean number of matches of the best and worst players in the population (by fitness) as well as the population average.
+    The highest and lowest number of matches by solutions in the popualtion are also plotted.
+    Args:
+        dir_path (String): The directory of the player trained under NEAT.
+        games_per_solution(int): The number of games each solution plays in its fitness calculation. Default set to 10.
+    Returns: None
+    """
+    #Load the paths of each generation's game file from the relevant directory
     files = glob.glob('%s/game_files/*.txt' % dir_path)
     best_solution, worst_solution, pop_av, highest, lowest = [],[],[], [], []
 
     for game_file in files:
         with open(game_file, "r") as f:
             games = f.read().split('\n\n')[:-1]
+        #Extracts the scores and number of matches from each file
         all_scores = [Golf_Analyser.extract_scores(game + '\n') for game in games]
         all_matches = [extract_matches_round(Golf_Analyser.extract_hands(game + '\n')) for game in games]
 
@@ -61,20 +101,31 @@ def plot_mean_number_of_matches(dir_path, games_per_solution=10):
         solution_matches = [np.sum(sol) for sol in solution_matches]
         
         mean_solution_scores = np.mean(np.amin(solution_scores, axis=2), axis=1)
+        
+        #By using the scores, locate the best and worst solution at each generation and add their metrics
+        best_solution.append(float(solution_matches[np.argmin(mean_solution_scores)]/2))
+        worst_solution.append(float(solution_matches[np.argmax(mean_solution_scores)]/2))
 
-        best_solution.append(float(solution_matches[np.argmin(mean_solution_scores)]/(9*2)))
-        worst_solution.append(float(solution_matches[np.argmax(mean_solution_scores)]/(9*2)))
-
-        lowest.append(float(min(solution_matches)/(9*2)))
-        highest.append(float(max(solution_matches)/(9*2)))
-        pop_av.append(float(np.mean(solution_matches)/(9*2)))
+        #Extract the highest, lowest and average number of matches obtained by solutions in the population
+        lowest.append(float(min(solution_matches)/2))
+        highest.append(float(max(solution_matches)/2))
+        pop_av.append(float(np.mean(solution_matches)/2))
         
     Golf_Analyser.plot_data(list(zip(best_solution, worst_solution, pop_av, highest, lowest)), 5, "Generations", "Average Matches per Game", "Average Matches per Game over Generations", "Best Solution", "Worst Solution", "Population Average", "Population Highest", "Population Lowest")
 
 def extract_matches_round(hands):
+    """
+    Calculates the number of matches made in an array of hands of both players and opponents.
+    Args:
+        hands([[String, String]]): An array containing the hands of both player and opponent for each round
+    Returns: An array containing the number of matches in each hand
+    """
     matches = []
     for h in hands:
+        #Converts the characters to their ASCII value
         asc_vals_player = [ord(card) for card in h[0]]
+
+        #Sums the number of matches that appear in the hand. Special cases for catching matching jokers
         player_matches = sum((asc_vals_player[i] > 116 and asc_vals_player[i+3] > 116) 
                         or (asc_vals_player[i] < 117 and asc_vals_player[i+3] < 117 and (asc_vals_player[i] - asc_vals_player[i+3])%13==0) for i in range(3))
                 
@@ -84,4 +135,8 @@ def extract_matches_round(hands):
         matches.append([player_matches, opponent_matches])
     return matches
 
-plot_mean_number_of_matches("games/neat/one_hot_state_and_hand_3")
+import sys
+p = sys.argv[1] #"games/neat/one_hot_hand_2"
+#plot_mean_scores(p)
+#plot_mean_number_of_turns(p)
+plot_mean_number_of_matches(p)
--- function_approximator.py
+++ function_approximator.py
@@ -1,3 +1,7 @@
+"""
+---function_approximator.py---
+Contains the function approximator classes as well as functions that can convert game states to the different input representations.
+"""
 from abc import ABC, abstractmethod
 import random
 from neural_net import Neural_Network
@@ -5,74 +9,55 @@ import numpy as np
 import neat
 
 class Func_Approx(ABC):
+    """
+    The function approximator of a player, which allows for game states to be evaluated and assigned a numerical value.
+    Attributes:
+        state_function (function): A function which converts the game state into a suitable format for the neural network.
+        network (Neural_Network): The neural network that calculates the value of the game state once the input has been converted.
+    """
 
     def __init__(self, state_function, network=None):
         self.state_function = state_function
         self.network = network
-
 
     @abstractmethod
     def value_of_state(self, state):
         pass
 
 class CoEvo_Func_Approx(Func_Approx):
+    """
+    The function approximator for players trained with the coevolution algorithm.
+    Contains the relevent methods for updating and mutating the weights of the network.
+    Attributes:
+        n_hidden (int): The number of nodes in the hidden layer of the neural network.
+        state_function (function): A function which converts the game state into a suitable format for the neural network.
+    """
     def __init__(self, n_hidden, state_function):
+        #Use the state function to calculate the number of inputs
         n_input = len(state_function([0]*54))
         network = Neural_Network(n_input, n_hidden, 1)
         super().__init__(state_function, network)
 
     def update(self, opposing_func_approx, crossover=0.05):
+        """
+        Updates the weights of the neural network in accordance with the coevolution algorithm.
+        Moves the weights of the network in the direction of opposing_func_approx by the crossover rate.
+        Args:
+            opposing_func_approx (CoEvo_Func_Approx): The opposing (better) function approximator of the opponent
+            crossover (float): The crossover rate of the update, the percentage in which the weights are moved in the direction of the opposing_func_approx.
+        Returns: None
+        """
         self.network.W_hidden = (opposing_func_approx.network.W_hidden - self.network.W_hidden)*crossover + self.network.W_hidden
         self.network.W_output = (opposing_func_approx.network.W_output - self.network.W_output)*crossover + self.network.W_output
         
     def value_of_state(self, state):
-        #one_hot_state = []
-        #for card_pos in state:
-        #    one_hot_card_pos = [0]*9
-        #    one_hot_card_pos[card_pos] = 1
-        #    one_hot_state += one_hot_card_pos
-        #return self.network.feedforward(np.reshape(one_hot_state, (1, len(one_hot_state))))[0][0]
-
-        #return self.network.feedforward(np.reshape(state, (1, len(state))) +1)[0][0]
-
-        #input_state = np.reshape([1 if s>=0 else s for s in state], (1, len(state))) + 1
-        #return self.network.feedforward(input_state)[0][0]
-
-        #opp, dis, unk, hand = [0]*54, [0]*54, [0]*54, [0]*(6*15)
-        #for i in range(len(state)):
-        #    if state[i] == -3:
-        #        opp[i] = 1
-        #    elif state[i] == -2:
-        #        dis[i] = 1
-        #    elif state[i] == -1:
-        #        unk[i] = 1
-        #for i in range(6):
-        #    try:
-        #        ind = state.index(i)
-        #        if ind <= 51:
-        #            v = ind +1 - (ind//13)*13
-        #            hand[i*15 + v] = 1
-        #        else:
-        #            hand[i*15 + 14] = 1
-        #    except ValueError:
-        #        hand[i*15] = 1
-            
-        #input_state = opp + dis + unk + hand
-        #return self.network.feedforward(np.reshape(input_state, (1, len(input_state))))[0][0]
-
-        #hand = [0]*(6*15)
-        #for i in range(6):
-        #    try:
-        #        ind = state.index(i)
-        #        if ind <= 51:
-        #            v = ind +1 - (ind//13)*13
-        #            hand[i*15 + v] = 1
-        #        else:
-        #            hand[i*15 + 14] = 1
-        #    except ValueError:
-        #        hand[i*15] = 1
-        #return self.network.feedforward(np.reshape(hand, (1, len(hand))))[0][0]
-
+        """
+        Calculates the value of the given game state via the neural network.
+        The state_function is used to convert the input into a suitable format for the network.
+        Args:
+            state ([int]): The game state to be evaluated.
+        Returns: A float which denotes the value of the given game state.
+        """
         input_state = self.state_function(state)
         return self.network.feedforward(np.reshape(input_state, (1, len(input_state))))[0][0]
 
@@ -88,49 +73,78 @@ class CoEvo_Func_Approx(Func_Approx):
         self.network.W_output += np.random.normal(mean, sd, self.network.W_output.shape)
 
 class NEAT_Func_Approx(Func_Approx):
+    """
+    The function approximator for players trained with the NEAT algorithm.
+    Attributes:
+        state_function (function): A function which converts the game state into a suitable format for the neural network.
+        network (Neural_Network): The neural network that calculates the value of the game state once the input has been converted.
+    """
     def __init__(self, state_function, network):
         super().__init__(state_function, network)
 
     def value_of_state(self, state):
-        #hand = [0]*(6*15)
-        #for i in range(6):
-        #    try:
-        #        ind = state.index(i)
-        #        if ind <= 51:
-        #            v = ind +1 - (ind//13)*13
-        #            hand[i*15 + v] = 1
-        #        else:
-        #            hand[i*15 + 14] = 1
-        #    except ValueError:
-        #        hand[i*15] = 1
-        #return self.network.activate(hand)[0]
+        """
+        Calculates the value of the given game state via the neural network.
+        The state_function is used to convert the input into a suitable format for the network.
+        Args:
+            state ([int]): The game state to be evaluated.
+        Returns: A float which denotes the value of the given game state.
+        """
         input_state = self.state_function(state)
         return self.network.activate(input_state)[0] 
 
 def one_hot_hand(state):
+    """
+    Converts the given game state into the one hot hand input representation.
+    This input representation contains only in values of the cards in the player's hand using one hot encoding.
+    Args:
+        state([int]): The game state to be converted to the representation.
+    Returns: An int array containing the new representation of the state.
+    """
     hand = [0]*(6*15)
     for i in range(6):
         try:
+            #Attempt to find each card in the players hand in the given game state
             ind = state.index(i)
+            #If a card does, calculate its value and update the relevant element of the representation
             if ind <= 51:
                 v = ind +1 - (ind//13)*13
                 hand[i*15 + v] = 1
             else:
                 hand[i*15 + 14] = 1
+        #If the card is not in the player hand, update the relevant element of the representation
         except ValueError:
             hand[i*15] = 1
     return hand
 
 def one_hot_state(state):
+    """
+    Converts the given game state into the one hot state input representation.
+    This input representation denotes each card in the game and where it is located if known (values of -3 to 5) using one hot encoding.
+    Args:
+        state([int]): The game state to be converted to the representation.
+    Returns: An int array containing the new representation of the state.
+    """
     one_hot_state = []
     for card_pos in state:
         one_hot_card_pos = [0]*9
+        #Update index of array based on card location (values of -3 to 5)
         one_hot_card_pos[card_pos] = 1
         one_hot_state += one_hot_card_pos
     return one_hot_state
 
 def one_hot_state_and_hand(state):
+    """
+    Converts the given game state into the one hot state and hand input representation.
+    This input representation denotes each card in the game and where it is located if known (values of -3 to 5) using one hot encoding.
+    It makes use of the one hot hand representation to represent cards in the hand of the player.
+    Args:
+        state([int]): The game state to be converted to the representation.
+    Returns: An int array containing the new representation of the state.
+    """
     opp, dis, unk, hand = [0]*54, [0]*54, [0]*54, [0]*(6*15)
+    
+    #Add each card in the opponents hand, discard pile, or uknown to the relevant arrays
     for i in range(len(state)):
         if state[i] == -3:
             opp[i] = 1
@@ -138,16 +152,21 @@ def one_hot_state_and_hand(state):
             dis[i] = 1
         elif state[i] == -1:
             unk[i] = 1
+
     for i in range(6):
         try:
+            #Attempt to find each card in the players hand in the given game state
             ind = state.index(i)
+            #If a card does, calculate its value and update the relevant element of the representation
             if ind <= 51:
                 v = ind +1 - (ind//13)*13
                 hand[i*15 + v] = 1
             else:
                 hand[i*15 + 14] = 1
+        #If the card is not in the player hand, update the relevant element of the representation
         except ValueError:
             hand[i*15] = 1
-            
+    
+    #Recombine separate arrays to form full input
     input_state = opp + dis + unk + hand
     return input_state
